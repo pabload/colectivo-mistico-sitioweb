4f2043407c10f66a9311a26b86dcba8e9c554acd
unknown <juanpabload98@gmail.com>
2021-03-13 22:25:14 -0700

    mejoras en index

--- components/index-page/mainsectionindex/mainsectionindex.js
+++ components/index-page/mainsectionindex/mainsectionindex.js
@@ -4,10 +4,10 @@ import { Animated } from "react-animated-css";
 export const Mainsectionindex = () => {
     return (
         <main className={Styles.mainContainer}> 
-            <img className={Styles.svgWave} src="./assets/wavemainsectionindex.svg" alt=""/>
-            <div className={`container ${Styles.infoContainer}`}>
-                <div className="row h-100 justify-content-center align-items-center">
-                    <div className="col-lg-8 col-md-6">
+           
+            <div className={` ${Styles.infoContainer}`}>
+                <div className="row justify-content-center align-items-center">
+                    <div className="col-lg-6 col-md-6">
                         <Animated animationIn="fadeInLeft" animationOut="fadeOut" isVisible={true}>
                             <h1 className={Styles.mainTitle}>¡Diferentes propuestas, un solo lugar!</h1>
                             <p className={Styles.subTitle}>Accesorios, productos artesanales, cerámica y mas</p>
