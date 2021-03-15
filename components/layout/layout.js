a4253e16dc3e939bfee0b4791f3cfdc688d1cae6
unknown <juanpabload98@gmail.com>
2021-03-15 16:57:46 -0700

    se agregago meta google

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -7,15 +7,16 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
+        <meta name="google-site-verification" content="8gytC6GBrAXSqWamQP51Sf-Cwsg6fiyL7UdipkGliVA" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossOrigin="anonymous"></link>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       <Navbar />
       <div className={styles.contetcontainer}>
         {children}
-        <Footer/>
+        <Footer />
       </div>
     </>
   )
